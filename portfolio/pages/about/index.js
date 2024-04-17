348bf0d20f07f8198380bef7d835cdf39b128a6b
nivasreddy99 <srinivasreddychitukula16@gmail.com>
2024-04-17 17:18:17 -0500

    wefgh

---
+++ portfolio/pages/about/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import React from 'react';
+import React from 'react';
+import React from 'react';
+import './index.css';
+
+const AboutPage = () => {
+    return (
+        <div className="about-page">
+            <h1>About Page</h1>
+            <p>Welcome to my portfolio website!</p>
+        </div>
+    );
+};
+
+export default AboutPage;
